9f5057bb1cec8f4c87f33bd84b98449c0991a828
Sam Craig <craigsamm@gmail.com>
2018-06-12 14:25:13 -0400

    Avatar CSS
    This should maybe be a separate component due to all the CSS (also the static version seemed to imply that)
    Either way, it works for now

--- src/UserInfo.js
+++ src/UserInfo.js
@@ -25,6 +25,8 @@ const styles = {
         marginRight: "0.5rem",
         width: "40px",
         height: "40px",
+        fontSize: "1rem",
+        borderRadius: "20px",
         backgroundImage: "url(https://avatars1.githubusercontent.com/u/3077078?s=400&u=241e48febff57a2d9daf4940870f1a0cc84979fc&v=4)",
         backgroundSize: "cover"
     },
