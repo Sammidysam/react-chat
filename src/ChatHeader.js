662548dff21933a27d017fd65ec94d91e5d3bcb7
Sam Craig <craigsamm@gmail.com>
2018-06-12 14:32:33 -0400

    ChatHeader CSS

--- src/ChatHeader.js
+++ src/ChatHeader.js
@@ -2,13 +2,33 @@ import React from "react"
 
 const ChatHeader = ({name, description}) => {
     return (
-        <div className="ChatHeader">
+        <div className="ChatHeader" style={styles.chatHeader}>
             <div className="roomInfo">
-                <h2>#{name}</h2>
-                <p>{description}</p>
+                <h2 style={styles.roomH2}>#{name}</h2>
+                <p style={styles.roomP}>{description}</p>
             </div>
         </div>
     )    
+}
+
+const styles = {
+    chatHeader: {
+        backgroundColor: "#f3f3f3",
+        borderBottom: "1px solid #ccc",
+        height: "3rem",
+        padding: "0 1rem",
+        display: "flex",
+        alignItems: "center"
+    },
+    roomH2: {
+        fontSize: "1.1rem",
+        margin: 0
+    },
+    roomP: {
+        color: "#999",
+        margin: 0,
+        fontSize: "0.8rem"
+    }
 }
 
 export default ChatHeader
