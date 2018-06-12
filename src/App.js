d726f5e9f98ebb9f6d419e553681a2b27e6f5a95
Sam Craig <craigsamm@gmail.com>
2018-06-12 09:28:10 -0400

    Add main component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+
+import Main from './Main'
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>React Chat</h1>
+        <Main />
       </div>
     );
   }
