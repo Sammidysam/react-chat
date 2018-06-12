47b861d0d7f824a83107091ca1a42c899d385ac8
Sam Craig <craigsamm@gmail.com>
2018-06-12 14:59:58 -0400

    Move UserInfo a rule out of JS to make it work
    Tomorrow in class I think we will tweak this

--- src/UserInfo.js
+++ src/UserInfo.js
@@ -30,7 +30,6 @@ const styles = {
         border: 0,
         padding: 0,
         backgroundColor: "transparent",
-        color: "rgba(255, 255, 255, 0.6)",
         fontSize: "1.2rem",
         transition: "color 0.25s ease-out"
     }
