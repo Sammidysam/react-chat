b9c67eef17916bae0c0647b4c347ab438dd9e146
Sam Craig <craigsamm@gmail.com>
2018-06-20 11:41:29 -0400

    Style the messages and add a new (nonfunctional) emoji button

--- src/Message.js
+++ src/Message.js
@@ -1,32 +1,57 @@
 import React from "react"
+import { StyleSheet, css } from "aphrodite"
 
 import Avatar from "./Avatar"
 import Metadata from "./Metadata"
 
 const Message = ({message}) => {
     return (
-        <div className="Message" style={styles.message}>
+        <div className={`Message ${css(styles.message)}`}>
           <Avatar user={message.user} />
-          <div className="details" style={styles.details}>
+          <div className={`${css(styles.details)}`}>
             <Metadata message={message} />
             <div className="body">
               {message.body}
             </div>
+            <button className={`reactionButton ${css(styles.reactionButton)}`}>
+                <i className="far fa-smile"></i>
+            </button>
           </div>
         </div>
     )    
 }
 
-const styles = {
+const styles = StyleSheet.create({
     message: {
         display: "flex",
         marginTop: "1rem",
-        padding: "0 1rem"
+        padding: "0.2rem 1rem",
+        position: "relative",
+
+        ":hover": {
+            backgroundColor: "#f6f6f6"
+        }
     },
     details: {
         flex: 1,
         paddingLeft: "0.5rem"
+    },
+    reactionButton: {
+        cursor: "pointer",
+        border: 0,
+        outline: 0,
+        backgroundColor: "transparent",
+        padding: 0,
+        color: "#ccc",
+        fontSize: "1rem",
+        position: "absolute",
+        top: "0.5rem",
+        right: "0.5rem",
+
+        ":hover": {
+            color: "blue"
+        }
     }
-}
+})
 
 export default Message
