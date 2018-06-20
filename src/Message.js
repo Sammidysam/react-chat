35012502e2ba243ea4acb81e71bc9206b12b17f0
Sam Craig <craigsamm@gmail.com>
2018-06-20 11:48:24 -0400

    Toggle Picker on emoji button click

--- src/Message.js
+++ src/Message.js
@@ -1,24 +1,42 @@
-import React from "react"
+import React, { Component } from "react"
 import { StyleSheet, css } from "aphrodite"
+import { Picker } from "emoji-mart"
 
 import Avatar from "./Avatar"
 import Metadata from "./Metadata"
 
-const Message = ({message}) => {
-    return (
-        <div className={`Message ${css(styles.message)}`}>
-          <Avatar user={message.user} />
-          <div className={`${css(styles.details)}`}>
-            <Metadata message={message} />
-            <div className="body">
-              {message.body}
+import "emoji-mart/css/emoji-mart.css"
+
+class Message extends Component {
+    constructor () {
+        super()
+
+        this.state = {
+            showPicker: false
+        }
+    }
+
+    togglePicker = () => {
+        this.setState({ showPicker: !this.state.showPicker })
+    }
+
+    render () {
+        return (
+            <div className={`Message ${css(styles.message)}`}>
+            <Avatar user={this.props.message.user} />
+            <div className={`${css(styles.details)}`}>
+                <Metadata message={this.props.message} />
+                <div className="body">
+                {this.props.message.body}
+                </div>
+                <button className={`reactionButton ${css(styles.reactionButton)}`} onClick={this.togglePicker}>
+                    <i className="far fa-smile"></i>
+                </button>
             </div>
-            <button className={`reactionButton ${css(styles.reactionButton)}`}>
-                <i className="far fa-smile"></i>
-            </button>
-          </div>
-        </div>
-    )    
+            {this.state.showPicker && <Picker showPreview={false} />}
+            </div>
+        )
+    }
 }
 
 const styles = StyleSheet.create({
